f18435220bab62ecbab296164d24514a1f9ddc93
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-08-21 22:33:30 +0300

    Summary

--- src/zappitProject/urls.py
+++ src/zappitProject/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from posts.views import PostList
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/posts/', PostList.as_view()),
 ]
