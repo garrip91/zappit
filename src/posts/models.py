c3ac7e89dbd3e8f9af20fac24e40d4bd7d78f5ac
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-08-21 20:55:30 +0300

    Summary

--- src/posts/models.py
+++ src/posts/models.py
@@ -8,4 +8,14 @@ class Post(models.Model):
 
     title = models.CharField(max_length=100)
     url = models.URLField()
-    poster = models.ForeignKey(User, )
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created']
+        
+        
+class Vote(models.Model):
+
+    voter = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
